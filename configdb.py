a4e28ceb4bdbd29eefc92552ae419f1af4c9d295
KumarShresth7 <kumarshresth2004@gmail.com>
2025-01-30 16:33:44 +0530

    db integrated successfully

--- configdb.py
+++ configdb.py
@@ -10,6 +10,7 @@ COLLECTION_NAME = "inventory"
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client[DATABASE_NAME]
 inventory_collection = db[COLLECTION_NAME]
+order_collection = db["orders"]
 
 dummy_inventory = [
     {"product": "iPhone 15", "quantity": 5},
@@ -17,5 +18,11 @@ dummy_inventory = [
     {"product": "iPad Air", "quantity": 10},
 ]
 
-inventory_collection.insert_many(dummy_inventory)
+dummy_orders = [
+    {"product": "iPhone 15", "quantity": 3},
+    {"product": "MacBook Pro", "quantity": 2},
+    {"product": "iPad Air", "quantity": 5},
+]
+
+# inventory_collection.insert_many(dummy_inventory)
 
