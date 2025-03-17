f4423ffcba9c981c5842f077b58b78a828e928ec
KumarShresth7 <kumarshresth2004@gmail.com>
2025-03-17 10:01:34 +0530

    #

--- python/flask/chatbot.py
+++ python/flask/chatbot.py
@@ -29,7 +29,7 @@ chat_history_collection = db["chat_history"]
 
 # Google Cloud Configuration
 PROJECT_ID = os.getenv("GOOGLE_PROJECT_ID")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\vedan\Downloads\EmailAutomation\server\fresh-airfoil-445517-q1-0df53973cc7e.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"D:\Deloitte\Prototype\python\services\fresh-airfoil-445517-q1-0df53973cc7e.json"
 aiplatform.init(project=PROJECT_ID, location="us-central1")
 embeddings = VertexAIEmbeddings(model="text-embedding-004", project=PROJECT_ID)
 
