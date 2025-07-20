2f9419752df9a429d9d7290976c790190b905df0
Vedant Mahajan <vedantmahajan271@gmail.com>
2025-07-20 13:15:29 +0530

    Update and Delete Inventory Item

--- flow-pilot/app/api/inventory/[id]/route.ts
+++ flow-pilot/app/api/inventory/[id]/route.ts
@@ -1,10 +1,43 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Inventory } from "@/models/Inventory";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     try {
         const id = await params.id;
         const inventory = await Inventory.findById(id);
+        return NextResponse.json(inventory);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.error();
+    }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const id = await params.id;
+
+        if(!id){
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+
+        const data = await request.json();
+        const inventory = await Inventory.findByIdAndUpdate(id, data);
+        return NextResponse.json(inventory);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.error();
+    }
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const id = await params.id;
+
+        if(!id){
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+
+        const inventory = await Inventory.findByIdAndDelete(id);
         return NextResponse.json(inventory);
     } catch (error) {
         console.error(error);
