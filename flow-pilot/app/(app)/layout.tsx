9e838353d9f0f7ae11932641580e5d1352bc40ca
Vedant Mahajan <vedantmahajan271@gmail.com>
2025-07-20 13:06:54 +0530

    Theme Update

--- flow-pilot/app/(app)/layout.tsx
+++ flow-pilot/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="flex h-screen overflow-hidden bg-black w-full">
+        <div className="flex h-screen overflow-hidden w-full">
             <Sidebar />
             <main className="flex-1 overflow-y-auto">
                 {children}
